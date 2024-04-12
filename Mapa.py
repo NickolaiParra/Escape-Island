5209d79e33bd4802f0e303a043f75337ec4216d7
David Nickolai Parra Ariza <dparraar@unal.edu.co>
2024-04-11 22:36:19 -0500

    Se añaden dos nuevos personajes

--- Mapa.py
+++ Mapa.py
@@ -5,24 +5,6 @@ pygame.init()
 
 from Módulos import Escape_Island as EI
 
-#Cargamos las hojas de sprites del personaje 1
-quieto_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\quieto_derecha.png")
-quieto_derecha = pygame.transform.scale(quieto_derecha, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
-quieto_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\quieto_izquierda.png")
-quieto_izquierda = pygame.transform.scale(quieto_izquierda, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
-correr_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\correr_derecha.png")
-caminar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\caminar_derecha.png")
-caminar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\caminar_izquierda.png")
-correr_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\correr_izquierda.png")
-ataque1_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque1_derecha.png")
-ataque1_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque1_izquierda.png")
-ataque3_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque3_derecha.png")
-ataque3_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque3_izquierda.png")
-saltar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\saltar_derecha.png")
-saltar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\saltar_izquierda.png")
-muerte = pygame.image.load("Imagenes\Sprites\Personaje_1\Muerte.png")
-defensa = pygame.image.load("Imagenes\Sprites\Personaje_1\Defensa.png")
-
 def cargar_sprites(hoja_sprites, ancho_sprite, alto_sprite, espacio_entre_sprites):
     """Esta función toma como argumento la hoja de sprites, el ancho de cada sprite, el alto de cada sprite, el espacio entre sprites y retorna cada sprite saparado individualmente en una lista"""
     sprites = []
@@ -33,17 +15,91 @@ def cargar_sprites(hoja_sprites, ancho_sprite, alto_sprite, espacio_entre_sprite
         sprites.append(cuadro)
     return sprites
 
-#Extraemos los sprites individuales del personaje 1
-sprites_correr_derecha = cargar_sprites(correr_derecha, correr_derecha.get_width()//8, 78, 128)
-sprites_caminar_derecha = cargar_sprites(caminar_derecha, caminar_derecha.get_width()//8, 84, 128)
-sprites_saltar_derecha = cargar_sprites(saltar_derecha, saltar_derecha.get_width()//10, 87, 128)
-sprites_saltar_izquierda = cargar_sprites(saltar_izquierda, saltar_izquierda.get_width()//10, 87, 128)
-sprites_caminar_izquierda = cargar_sprites(caminar_izquierda, caminar_izquierda.get_width()//8, 84, 128)
-sprites_correr_izquierda = cargar_sprites(correr_izquierda, correr_izquierda.get_width()//8, 78, 128)
-sprites_ataque3_derecha = cargar_sprites(ataque3_derecha, ataque3_derecha.get_width()//4, 81, 128)
-sprites_ataque3_izquierda = cargar_sprites(ataque3_izquierda, ataque3_izquierda.get_width()//4, 81, 128)
-sprites_ataque1_derecha = cargar_sprites(ataque1_derecha, ataque1_derecha.get_width()//4, 84, 128)
-sprites_ataque1_izquierda = cargar_sprites(ataque1_izquierda, ataque1_izquierda.get_width()//4, 84, 128)
+seleccion = 3
+
+if seleccion == 1:
+    #Cargamos las hojas de sprites del personaje 1
+    quieto_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\quieto_derecha.png")
+    quieto_derecha = pygame.transform.scale(quieto_derecha, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
+    quieto_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\quieto_izquierda.png")
+    quieto_izquierda = pygame.transform.scale(quieto_izquierda, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
+    correr_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\correr_derecha.png")
+    caminar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\caminar_derecha.png")
+    caminar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\caminar_izquierda.png")
+    correr_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\correr_izquierda.png")
+    ataque1_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque1_derecha.png")
+    ataque1_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque1_izquierda.png")
+    ataque3_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque3_derecha.png")
+    ataque3_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\\ataque3_izquierda.png")
+    saltar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_1\saltar_derecha.png")
+    saltar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_1\saltar_izquierda.png")
+    #Extraemos los sprites individuales del personaje 1
+    sprites_correr_derecha = cargar_sprites(correr_derecha, correr_derecha.get_width()//8, 78, 128)
+    sprites_caminar_derecha = cargar_sprites(caminar_derecha, caminar_derecha.get_width()//8, 84, 128)
+    sprites_saltar_derecha = cargar_sprites(saltar_derecha, saltar_derecha.get_width()//10, 87, 128)
+    sprites_saltar_izquierda = cargar_sprites(saltar_izquierda, saltar_izquierda.get_width()//10, 87, 128)
+    sprites_caminar_izquierda = cargar_sprites(caminar_izquierda, caminar_izquierda.get_width()//8, 84, 128)
+    sprites_correr_izquierda = cargar_sprites(correr_izquierda, correr_izquierda.get_width()//8, 78, 128)
+    sprites_ataque3_derecha = cargar_sprites(ataque3_derecha, ataque3_derecha.get_width()//4, 81, 128)
+    sprites_ataque3_izquierda = cargar_sprites(ataque3_izquierda, ataque3_izquierda.get_width()//4, 81, 128)
+    sprites_ataque1_derecha = cargar_sprites(ataque1_derecha, ataque1_derecha.get_width()//4, 84, 128)
+    sprites_ataque1_izquierda = cargar_sprites(ataque1_izquierda, ataque1_izquierda.get_width()//4, 84, 128)
+elif seleccion == 2:
+    #Cargamos las hojas de sprites del personaje 2
+    quieto_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\quieto_derecha.png")
+    quieto_derecha = pygame.transform.scale(quieto_derecha, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
+    quieto_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\quieto_izquierda.png")
+    quieto_izquierda = pygame.transform.scale(quieto_izquierda, (EI.ancho * 0.022, EI.alto * 0.09)) #Se adecua el sprite a un tamaño específico
+    correr_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\correr_derecha.png")
+    caminar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\caminar_derecha.png")
+    caminar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\caminar_izquierda.png")
+    correr_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\correr_izquierda.png")
+    ataque1_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\\ataque1_derecha.png")
+    ataque1_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\\ataque1_izquierda.png")
+    ataque3_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\\ataque3_derecha.png")
+    ataque3_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\\ataque3_izquierda.png")
+    saltar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_2\saltar_derecha.png")
+    saltar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_2\saltar_izquierda.png")
+    #Extraemos los sprites individuales del personaje 2
+    sprites_caminar_derecha = cargar_sprites(caminar_derecha, caminar_derecha.get_width()//8, 81, 128)
+    sprites_caminar_izquierda = cargar_sprites(caminar_izquierda, caminar_izquierda.get_width()//8, 81, 128)
+    sprites_saltar_derecha = cargar_sprites(saltar_derecha, saltar_derecha.get_width()//12, 85, 128)
+    sprites_saltar_izquierda = cargar_sprites(saltar_izquierda, saltar_izquierda.get_width()//12, 85, 128)
+    sprites_correr_izquierda = cargar_sprites(correr_izquierda, correr_izquierda.get_width()//8, 75, 128)
+    sprites_correr_derecha = cargar_sprites(correr_derecha, correr_derecha.get_width()//8, 75, 128)
+    sprites_ataque3_derecha = cargar_sprites(ataque3_derecha, ataque3_derecha.get_width()//4, 78, 128)
+    sprites_ataque3_izquierda = cargar_sprites(ataque3_izquierda, ataque3_izquierda.get_width()//4, 78, 128)
+    sprites_ataque1_derecha = cargar_sprites(ataque1_derecha, ataque1_derecha.get_width()//5, 80, 128)
+    sprites_ataque1_izquierda = cargar_sprites(ataque1_izquierda, ataque1_izquierda.get_width()//5, 80, 128)
+elif seleccion == 3:
+    #Cargamos las hojas de sprites del personaje 3
+    quieto_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\quieto_derecha.png")
+    quieto_derecha = pygame.transform.scale(quieto_derecha, (EI.ancho * 0.058, EI.alto * 0.088)) #Se adecua el sprite a un tamaño específico
+    quieto_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\quieto_izquierda.png")
+    quieto_izquierda = pygame.transform.scale(quieto_izquierda, (EI.ancho * 0.058, EI.alto * 0.088)) #Se adecua el sprite a un tamaño específico
+    correr_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\correr_derecha.png")
+    caminar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\caminar_derecha.png")
+    caminar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\caminar_izquierda.png")
+    correr_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\correr_izquierda.png")
+    ataque1_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\\ataque1_derecha.png")
+    ataque1_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\\ataque1_izquierda.png")
+    ataque3_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\\ataque3_derecha.png")
+    ataque3_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\\ataque3_izquierda.png")
+    saltar_derecha = pygame.image.load("Imagenes\Sprites\Personaje_3\saltar_derecha.png")
+    saltar_izquierda = pygame.image.load("Imagenes\Sprites\Personaje_3\saltar_izquierda.png")
+    #Extraemos los sprites individuales del personaje 3
+    sprites_caminar_derecha = cargar_sprites(caminar_derecha, caminar_derecha.get_width()//11, 80, 128)
+    sprites_caminar_izquierda = cargar_sprites(caminar_izquierda, caminar_izquierda.get_width()//11, 80, 128)
+    sprites_saltar_derecha = cargar_sprites(saltar_derecha, saltar_derecha.get_width()//11, 92, 128)
+    sprites_saltar_izquierda = cargar_sprites(saltar_izquierda, saltar_izquierda.get_width()//11, 92, 128)
+    sprites_correr_izquierda = cargar_sprites(correr_izquierda, correr_izquierda.get_width()//9, 81, 128)
+    sprites_correr_derecha = cargar_sprites(correr_derecha, correr_derecha.get_width()//9, 81, 128)
+    sprites_ataque3_derecha = cargar_sprites(ataque3_derecha, ataque3_derecha.get_width()//5, 95, 128)
+    sprites_ataque3_izquierda = cargar_sprites(ataque3_izquierda, ataque3_izquierda.get_width()//5, 95, 128)
+    sprites_ataque1_derecha = cargar_sprites(ataque1_derecha, ataque1_derecha.get_width()//6, 90, 128)
+    sprites_ataque1_izquierda = cargar_sprites(ataque1_izquierda, ataque1_izquierda.get_width()//6, 90, 128)
+
+
 
 mapa_avion_ajustado =  pygame.transform.scale(EI.mapa_avion, (EI.ancho, EI.alto)) #Ajustamos el mapa al tamaño de la pantalla
 EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Mostramos el mapa
@@ -103,10 +159,11 @@ while True:
 
     #Si se presiona la tecla "x", entonces el personaje realiza el ataque 1         
     if ataque1 and (anterior == "derecha" or anterior == None):
+        restar = 0 if seleccion == 3 else EI.ancho * 0.02
         for sprite in sprites_ataque1_derecha:
             EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Se limpia la pantalla
             velocidad_personaje1 += EI.ancho * 0.003
-            EI.PANTALLA.blit(sprite, (velocidad_personaje1 - EI.ancho * 0.02, altura - EI.alto * 0.009))  #Se dibuja el sprite actual
+            EI.PANTALLA.blit(sprite, (velocidad_personaje1 - restar, altura - EI.alto * 0.009))  #Se dibuja el sprite actual
             pygame.display.flip()  #Se actualiza la pantalla
             pygame.time.delay(90)  #Se pausa la ejecución durante un breve período de tiempo
         ataque1 = False
@@ -123,10 +180,11 @@ while True:
 
     #Si se presiona la tecla "z", entonces el personaje realiza el ataque 3            
     if ataque3 and (anterior == "derecha" or anterior == None):
+        restar = 0 if seleccion == 3 else EI.ancho * 0.02
         for sprite in sprites_ataque3_derecha:
             EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Se limpia la pantalla
             velocidad_personaje1 += EI.ancho * 0.003
-            EI.PANTALLA.blit(sprite, (velocidad_personaje1 - EI.ancho * 0.02, altura - EI.alto * 0.009))  #Se dibuja el sprite actual
+            EI.PANTALLA.blit(sprite, (velocidad_personaje1 - restar, altura - EI.alto * 0.009))  #Se dibuja el sprite actual
             pygame.display.flip()  #Se actualiza la pantalla
             pygame.time.delay(90)  #Se pausa la ejecución durante un breve período de tiempo
         ataque3 = False
@@ -162,7 +220,6 @@ while True:
         saltando = False
 
     teclas = pygame.key.get_pressed() 
-
     #Si no se está presionando ninguna tecla, entonces el personaje se queda quieto
     if teclas[pygame.K_RIGHT] == False and (anterior == "derecha" or anterior == None):
         EI.PANTALLA.blit(quieto_derecha, (velocidad_personaje1 + EI.ancho * 0.027, altura))
@@ -192,65 +249,68 @@ while True:
         altura -= EI.alto * 0.01
         EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Se limpia la pantalla
         EI.PANTALLA.blit(quieto_izquierda, (velocidad_personaje1 + EI.ancho * 0.04, altura))
-
+    
+    
     #Si la tecla derecha está siendo presionada o "d", entonces el personaje camina hacia la derecha
     if (teclas[pygame.K_RIGHT] or teclas[pygame.K_d]) and (teclas[pygame.K_LSHIFT] or teclas[pygame.K_RSHIFT]) == False:
         EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Se limpia la pantalla
         velocidad_personaje1 += EI.ancho * 0.002 
+        sumar = EI.ancho * 0.015 if seleccion == 3 else 0
         anterior = "derecha"
         if estado_caminar_derecha == None or estado_caminar_derecha == 7:
-            EI.PANTALLA.blit(sprites_caminar_derecha[0], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[0], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 0
         elif estado_caminar_derecha == 0:
-            EI.PANTALLA.blit(sprites_caminar_derecha[1], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[1], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 1
         elif estado_caminar_derecha == 1:
-            EI.PANTALLA.blit(sprites_caminar_derecha[2], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[2], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 2
         elif estado_caminar_derecha == 2:
-            EI.PANTALLA.blit(sprites_caminar_derecha[3], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[3], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 3
         elif estado_caminar_derecha == 3:
-            EI.PANTALLA.blit(sprites_caminar_derecha[4], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[4], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 4
         elif estado_caminar_derecha == 4:
-            EI.PANTALLA.blit(sprites_caminar_derecha[5], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[5], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 5
         elif estado_caminar_derecha == 5:
-            EI.PANTALLA.blit(sprites_caminar_derecha[6], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[6], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 6
         elif estado_caminar_derecha == 6:
-            EI.PANTALLA.blit(sprites_caminar_derecha[7], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_derecha[7], (velocidad_personaje1 + sumar, altura))
             estado_caminar_derecha = 7
 
     #Si la tecla izquierda está siendo presionada o "a", entonces el personaje camina hacia la izquierda
     if (teclas[pygame.K_LEFT] or teclas[pygame.K_a]) and (teclas[pygame.K_LSHIFT] or teclas[pygame.K_RSHIFT]) == False:
         EI.PANTALLA.blit(mapa_avion_ajustado, (0, 0)) #Se limpia la pantalla
         velocidad_personaje1 -= EI.ancho * 0.002 
+        sumar = EI.ancho * 0.02 if seleccion == 3 else 0
         anterior = "izquierda"
         if estado_caminar_izquierda == None or estado_caminar_izquierda == 7:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[7], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[7], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 0
         elif estado_caminar_izquierda == 0:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[6], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[6], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 1
         elif estado_caminar_izquierda == 1:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[5], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[5], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 2
         elif estado_caminar_izquierda == 2:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[4], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[4], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 3
         elif estado_caminar_izquierda == 3:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[3], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[3], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 4
         elif estado_caminar_izquierda == 4:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[2], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[2], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 5
         elif estado_caminar_izquierda == 5:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[1], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[1], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 6
         elif estado_caminar_izquierda == 6:
-            EI.PANTALLA.blit(sprites_caminar_izquierda[0], (velocidad_personaje1, altura))
+            EI.PANTALLA.blit(sprites_caminar_izquierda[0], (velocidad_personaje1 + sumar, altura))
             estado_caminar_izquierda = 7
 
     #Si la tecla derecha o "d" y shift están siendo presionadas, entonces el personaje corre hacia la derecha
