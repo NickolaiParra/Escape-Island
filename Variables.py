512747341e5ff5832886d259eab4bdbbb8159aa4
Julián <jufepe2006@gmail.com>
2024-05-18 16:50:04 -0500

    Nivel de variables acabado -Se terminó los diálogos del nivel de variables, así como sus quices y examen. -Falta pasar el código de variables.py a Mapa.py

--- Variables.py
+++ Variables.py
@@ -156,19 +156,192 @@ while running:
         d = "Una cadena de texto."
         correcta = 2
         pregunta("Quiz de variables 1",[t1,"",""],a,b,c,d)
-        dialog_continue = False
-        #Cuando se responda correctamente:
-        #dialog_continue debe volver a True
-        #se le debe sumar 1 a num_dialog
-    elif num_dialog == 11:
+        #dialog_continue = False
+    elif num_dialog == -100: # Si se equivoca
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Eso no es correcto."
+        t2 = "Vamos a volver a repasar el concepto clave"
+        EI.mostrar_texto(personaje,t1,t2)
+        #Vuelve a num_dialog == 6
+    elif num_dialog == 11: # Si responde correctamente
         EI.PANTALLA.blit(variably,variably_rect)
         personaje = "Variably"
         t1 = "Bien hecho joven aventurero."
         t2 = "Haz completado exitosamente el primer paso para dominar las variables."
         t3 = "Ya que aprendimos la parte teórica, veamos como se aplica."
         EI.mostrar_texto(personaje,t1,t2,t3)
-        #Siguen los diálogos del segundo tutorial
-        
+    elif num_dialog == 12:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Para esto vamos a usar un lenguaje de programación que se llama python."
+        t2 = "Ya verás que es bastante intuitivo y fácil de usar."
+        EI.mostrar_texto(personaje,t1,t2)
+    elif num_dialog == 13:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Para hacer una variable primero debes ponerle un nombre."
+        t2 = "Generalmente el nombre que le pones está relacionado con lo que guarda dentro."
+        t3 = "Pero le puedes poner el nombre que más te parezca."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 14:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Por ejemplo si vas a escribir nombre (Una cadena de texto) puedes poner:"
+        t2 = "nombre = 'Juan'"
+        t3 = "Y así se guarda 'Juan' en la variable 'nombre'"
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 15:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "También puedes guardar números de la siguente manera:"
+        t2 = "numero_entero = 42"
+        t3 = "numero_decimal = 3.1416"
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 16:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "El último tipo de dato simple que puedes guardar es un booleano. "
+        t2 = "Este dato es un valor que puede ser verdadero o falso."
+        t3 = "El valor de verdadero se escribe como 'True' y falso como 'False'"
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 17:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Un ejemplo de esto es el siguiente:"
+        t2 = "valor = True"
+        t3 = "No se te olvide que los booleanos van en mayúscula en python."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 17:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Esos son los tipos más sencillos de datos que vas a encontrar."
+        t2 = "Y ya sabes como guardarlos en una variable para poder trabajar con ellos después."
+        t3 = "Durante tu viaje seguirás usándolos y prendiendo sobre estos."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 18:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Eso es todo por tu segunda y última lección aquí en la aldea."
+        t2 = "Te voy a hacer una pregunta para ver si entendiste bien la lección."
+        t3 = "Aquí vamos!"
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 19:
+        #Quiz de variables 2
+        EI.PANTALLA.blit(variably,variably_rect)
+        t1 = "Si yo quiero crear una variable que se llame 'fruta' y que guarde mi fruta favorita,"
+        t2 = "¿Cuál de las siguentes es la más correcta de nombrar la variable?"
+        t3 = "Ten en cuenta la manera en la que están escritas."
+        a = "'fresa' = fruta"
+        b = "'fruta' = manzana"
+        c = "kiwi = 'fruta'"
+        d = "fruta = 'banana'"
+        correcta = 4
+        pregunta("Quiz de variables 2",[t1,t2,t3],a,b,c,d)
+        #dialog_continue = False
+    elif num_dialog == -200: # Si se equivoca
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Eso no es correcto."
+        t2 = "Devólvamonos para repasarlo."
+        EI.mostrar_texto(personaje,t1,t2)
+        #Vuelve a num_dialog == 13
+    elif num_dialog == 20: # Si responde correctamente
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Muy bien."
+        t2 = "Me queda claro que estás dominando el arte de crear variables."
+        t3 = "Solo falta una última cosa que hacer."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 21:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Te vas a enfrentar al mayor reto que hayas visto hasta el momento"
+        t2 = "Voy a hacerte 5 preguntas sobre lo que aprendiste hasta ahora."
+        t3 = "Estarás solo y después vendré a ver cómo te fue."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 22:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Si pasas podrás irte."
+        t2 = "Si no tu tiempo aquí se alargará."
+        t3 = "¿Estás listo?"
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 23:
+        #Examen de variables
+        #Pregunta 1
+        t1 = "¿Para qué sirve una variable?"
+        a = "Para guardar información."
+        b = "Para hacer que un número varíe."
+        c = "Para despejar una ecuación matemática."
+        d = "Para ejecutar código."
+        correcta = 1
+        pregunta("Examen de variables",[t1,"",""],a,b,c,d)
+    elif num_dialog == 24:
+        #Pregunta 2
+        t1 = "¿Cuál es la sintaxis corecta para definir una variable en python?"
+        a = "variable = valor"
+        b = "valor = variable"
+        c = "variable == valor"
+        d = "valor == variable"
+        correcta = 3
+        pregunta("Examen de variables",[t1,"",""],a,b,c,d)
+    elif num_dialog == 25:
+        #Pregunta 3
+        t1 = "Si se define la siguiente variable:"
+        t2 = "ejecutando = True"
+        t3 = "¿De qué tipo es la información qué guarda?"
+        a = "Texto"
+        b = "Booleano"
+        c = "Número entero"
+        d = "Número decimal"
+        correcta = 2
+        pregunta("Examen de variables",[t1,t2,t3],a,b,c,d)
+    elif num_dialog == 26:
+        #Pregunta 4
+        t1 = "¿A qué corresponde el valor de la variable 'dulces' en el siguiente código?"
+        t2 = "dulces = True"
+        t3 = "dulces = 32"
+        a = "32"
+        b = "True"
+        c = "None"
+        d = "True 32"
+        correcta = 1
+        pregunta("Examen de variables",[t1,t2,t3],a,b,c,d)
+    elif num_dialog == 27:
+        #Pregunta 5
+        t1 = "¿Cuál es el nombre del lenguaje de programación que se ha estado usando?"
+        a = "c++"
+        b = "java"
+        c = "python"
+        d = "c"
+        correcta = 3
+        pregunta("Examen de variables",[t1,t2,t3],a,b,c,d)
+    elif num_dialog == -300: # Si no pasa
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Lo intentase."
+        t2 = "No pierdas la esperanza."
+        t3 = "Lo intentaremos otra vez."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+        #Vuelve a num_dialog == 13
+    elif num_dialog == 28: # Si pasa
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Felicidades."
+        t2 = "Eres un maestro de las variables."
+        t3 = "Te deseo lo mejor aventurero."
+        EI.mostrar_texto(personaje,t1,t2,t3)
+    elif num_dialog == 29:
+        EI.PANTALLA.blit(variably,variably_rect)
+        personaje = "Variably"
+        t1 = "Ya puedes proseguir con tu camino."
+        EI.mostrar_texto(personaje,t1)
+    elif num_dialog == 30:
+        EI.mostrar_texto("Escape Island","Saliendo de la aldea de variables",color=EI.AZUL)
+    elif num_dialog == 31:
+        EI.mostrar_texto("","")
+        #Vuelve al mapa principal
     else:
         EI.mostrar_texto("","")
     
@@ -182,11 +355,10 @@ while running:
             elif event.key == pygame.K_RIGHT and dialog_continue:
                 #Avanzar el diálogo
                 num_dialog += 1
-                print(num_dialog)
+                
             elif num_dialog >= 0 and event.key == pygame.K_LEFT and dialog_continue:
                 #Retroceder el diálogo
                 num_dialog -= 1
-                print(num_dialog)
     
     pygame.time.Clock().tick(60)
     pygame.display.update()
